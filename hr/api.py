66b126d3504feb8035ff752b651d4336bacb87f0
Kashif-Zeb <128372577+Kashif-Zeb@users.noreply.github.com>
2024-09-20 14:38:32 +0500

    Update api.py

--- hr/api.py
+++ hr/api.py
@@ -88,7 +88,7 @@ def create_app():
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'kashifzeb19@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'tqoy bzen jlxl kgyn'
+    app.config['MAIL_PASSWORD'] = ''
     app.config['MAIL_DEFAULT_SENDER'] = 'kashifzk216@gmail.com'
 
     mail.init_app(app)
